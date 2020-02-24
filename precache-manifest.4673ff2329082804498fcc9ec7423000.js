4b1c41dc084015d53ec9c949d1ca753c4ad668ab
Eric Batista <klose.eric31@gmail.com>
2020-02-24 15:02:43 -0300

    Updates

---
+++ precache-manifest.4673ff2329082804498fcc9ec7423000.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08241b3964d613995f00206e9c70889a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "559a692cd4c5de533130",
+    "url": "/static/css/main.8c64d37c.chunk.css"
+  },
+  {
+    "revision": "57228ee43b57e2529781",
+    "url": "/static/js/2.0e62cdaa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0e62cdaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559a692cd4c5de533130",
+    "url": "/static/js/main.47aec717.chunk.js"
+  },
+  {
+    "revision": "4e81e6700ec56eb4ae23",
+    "url": "/static/js/runtime-main.5eb8e6b3.js"
+  },
+  {
+    "revision": "48583f9a99bfd90625a4e31a578fc9ed",
+    "url": "/static/media/back.48583f9a.jpg"
+  },
+  {
+    "revision": "03002b3d242be2968a955a86bb19a00d",
+    "url": "/static/media/foto.03002b3d.jpg"
+  }
+]);
